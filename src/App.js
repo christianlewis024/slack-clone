3ee6401d70454a84cdebdb1be58ca91e121e12d4
Christian Lewis <christianlewis024@gmail.com>
2020-10-13 11:24:01 -0700

    top of sidebar in place with some css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from "./Header"
+import Sidebar from "./Sidebar"
 
 import './App.css';
 
@@ -8,7 +9,8 @@ function App() {
     <div className="app">    
      <Header/>
      <div className="app__body">
-       {/* sidebar */}
+      <Sidebar/> 
+    
      {/* react-router -> chat screen*/}
      </div>     
     </div>
---
+++ src/Sidebar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Sidebar.css"
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord"
+import CreateIcon from "@material-ui/icons/Create"
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebar__header">
+                <div className="sidebar__info">
+                <h2>Slack Workspace</h2>
+                <h3><FiberManualRecordIcon/>Christian L</h3>
+                </div>
+                <CreateIcon/>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
