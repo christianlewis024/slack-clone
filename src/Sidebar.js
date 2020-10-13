76bf3eb6403c6d1ebceb8009f0218c2c712e4878
Christian Lewis <christianlewis024@gmail.com>
2020-10-13 13:29:52 -0700

    can dynamically add channels from front end

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -54,7 +54,7 @@ function Sidebar() {
             <hr/>
             <SidebarOption  Icon={ExpandMoreIcon} title="Channels"/>
             <hr/>
-            <SidebarOption  Icon={AddIcon} addChannelOption="" title="Add Channel"/>
+            <SidebarOption  Icon={AddIcon} addChannelOption title="Add Channel"/>
             {/* connect to db and list all the channels */}
             {/* sidebarOption */}
             {channels.map(channel => (
