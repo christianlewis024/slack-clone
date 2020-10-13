3a9e353d65b582e360f49132c3257f34ad2a8a83
Christian Lewis <christianlewis024@gmail.com>
2020-10-13 14:18:01 -0700

    created login component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from "./Header"
 import Sidebar from "./Sidebar"
 import{ BrowserRouter as Router, Switch, Route} from "react-router-dom"
 import Chat from "./Chat"
+import Login from "./Login"
 
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState(null)
   return (    
     <div className="app">
       <Router>
+        {!user ? (
+          <h1>Login Page</h1>
+        ) : (
+          <>
+        
      <Header/>
      <div className="app__body">
       <Sidebar/>
@@ -25,6 +32,8 @@ function App() {
       </Switch>   
      {/* react-router -> chat screen*/}
      </div>
+     </>
+     )}
      </Router>     
     </div>
   );
